88f998091da443685f20a5deabb9476b3d0760cc
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-10-26 22:35:57 +0300

    added test for Utils

--- app/src/main/java/com/alexkaz/task2/util/Utils.java
+++ app/src/main/java/com/alexkaz/task2/util/Utils.java
@@ -7,33 +7,66 @@ public class Utils {
     private static String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
     public static String formatDate(String date){
+        if (date == null) throw new IllegalArgumentException();
+
         DateTime then = new DateTime(date);
         DateTime now = new DateTime();
 
         String result = "Updated ";
 
-        if (then.getYear() == now.getYear()){
-            if (then.getMonthOfYear() == now.getMonthOfYear()){
-                if (then.getDayOfMonth() == now.getDayOfMonth()){
-                    if (then.getHourOfDay() == now.getHourOfDay()){
-                        if (then.getMinuteOfHour() == now.getMinuteOfHour()){
-                            result += "a minute ago";
-                        } else result += (now.getMinuteOfHour() - then.getMinuteOfHour()) + " minutes ago";
-                    } else result += (now.getHourOfDay() - then.getHourOfDay()) + " hours ago";
-                } else result += (now.getDayOfMonth() - then.getDayOfMonth()) + " days ago";
-            } else result += "on " + then.getDayOfMonth() + " " + months[then.getMonthOfYear()-1];
-        } else result += "on " + then.getDayOfMonth() + " " + months[then.getMonthOfYear()-1] + " " + then.getYear();
+        long thenMillis = then.getMillis();
+        long nowMillis = now.getMillis();
 
-        return result;
+        long diff = nowMillis - thenMillis;
+
+        if (diff <= 1000L * 60L){
+            if (diff / 1000L == 1)
+                return result + "a second ago";
+            return result + diff / 1000L + " seconds ago";
+        }
+
+        if (diff <= 1000L * 60L * 60L){
+            if (diff / (1000L * 60L) == 1)
+                return result + "a minute ago";
+            return result + diff / (1000 * 60) + " minutes ago";
+        }
+
+        if (diff <= 1000L * 60L * 60L * 24L){
+            if (diff / (1000L * 60L * 60L) == 1)
+                return result + "an hour ago";
+            return result + diff / (1000L * 60L * 60L) + " hours ago";
+        }
+
+        if (diff <= (1000L * 60L * 60L * 24L * now.dayOfMonth().getMaximumValue())){
+            if (diff / (1000L * 60L * 60L * 24L) == 1)
+                return result + "a day ago";
+            return result + diff / (1000L * 60L * 60L * 24L) + " days ago";
+        }
+
+        if (then.getYear() == now.getYear())
+            return result + "on " + then.getDayOfMonth() + " " + months[then.getMonthOfYear()-1];
+        else
+            return result + "on " + then.getDayOfMonth() + " " + months[then.getMonthOfYear()-1] + " " + then.getYear();
     }
 
     public static String formatNumber(int number){
-        if (number <1_000)
+        if (number < 0) throw new IllegalArgumentException("Argument must be > 0 !");
+
+        if (number < 1_000)
             return String.valueOf(number);
-        if (number < 1_000_000)
-            return number / 1_000 + "k";
-        else
-            return number / 1_000_000 + "m";
+        if (number < 1_000_000){
+            int remainder = number % 1_000;
+            if (remainder >= 500)
+                return ((number / 1_000) + 1) + "k";
+            else
+                return number / 1_000 + "k";
+        } else{
+            int remainder = number % 1_000_000;
+            if (remainder >= 500_000)
+                return ((number / 1_000_000) + 1) + "m";
+            else
+                return number / 1_000_000 + "m";
+        }
     }
 
 }
---
+++ app/src/test/java/com/alexkaz/task2/TestUtils.java
@@ -0,0 +1,125 @@
+package com.alexkaz.task2;
+
+import com.alexkaz.task2.util.Utils;
+
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TestUtils {
+
+    @Test
+    public void test_formatNumber(){
+        String result = Utils.formatNumber(0);
+        assertThat(result, is("0"));
+
+        result = Utils.formatNumber(1);
+        assertThat(result, is("1"));
+
+        result = Utils.formatNumber(345);
+        assertThat(result, is("345"));
+
+        result = Utils.formatNumber(999);
+        assertThat(result, is("999"));
+
+        result = Utils.formatNumber(1000);
+        assertThat(result, is("1k"));
+
+        result = Utils.formatNumber(1900);
+        assertThat(result, is("2k"));
+
+        result = Utils.formatNumber(9500);
+        assertThat(result, is("10k"));
+
+        result = Utils.formatNumber(1_000_000);
+        assertThat(result, is("1m"));
+
+        result = Utils.formatNumber(2_500_000);
+        assertThat(result, is("3m"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_formatNumber_invalid_arg(){
+        Utils.formatNumber(-2);
+    }
+
+    @Test
+    public void test_formatDate(){
+        DateTime dateTime = new DateTime();
+
+        dateTime = dateTime.minus(new Period(0,0,25,0));
+
+        String result = Utils.formatDate(dateTime.toString());
+        assertThat(result, is("Updated 25 seconds ago"));
+
+        dateTime = new DateTime();
+        dateTime = dateTime.minus(new Period(0,0,1,0));
+        result = Utils.formatDate(dateTime.toString());
+        assertThat(result, is("Updated a second ago"));
+
+        dateTime = new DateTime();
+        dateTime = dateTime.minus(new Period(0,0,59,0));
+        result = Utils.formatDate(dateTime.toString());
+        assertThat(result, is("Updated 59 seconds ago"));
+
+        dateTime = new DateTime();
+        dateTime = dateTime.minus(new Period(0,1,0,0));
+        result = Utils.formatDate(dateTime.toString());
+        assertThat(result, is("Updated a minute ago"));
+
+        dateTime = new DateTime();
+        dateTime = dateTime.minus(new Period(0,10,0,0));
+        result = Utils.formatDate(dateTime.toString());
+        assertThat(result, is("Updated 10 minutes ago"));
+
+        dateTime = new DateTime();
+        dateTime = dateTime.minus(new Period(0,45,0,0));
+        result = Utils.formatDate(dateTime.toString());
+        assertThat(result, is("Updated 45 minutes ago"));
+
+        dateTime = new DateTime();
+        dateTime = dateTime.minus(new Period(1,0,0,0));
+        result = Utils.formatDate(dateTime.toString());
+        assertThat(result, is("Updated an hour ago"));
+
+        dateTime = new DateTime();
+        dateTime = dateTime.minus(new Period(0,0,0,1,0,0,0,0));
+        result = Utils.formatDate(dateTime.toString());
+        assertThat(result, is("Updated a day ago"));
+
+        dateTime = new DateTime();
+        dateTime = dateTime.minus(new Period(0,0,0,30,0,0,0,0));
+        result = Utils.formatDate(dateTime.toString());
+        assertThat(result, is("Updated 30 days ago"));
+
+        dateTime = new DateTime();
+        dateTime = dateTime.minus(new Period(0,2,0,0,0,0,0,0));
+        result = Utils.formatDate(dateTime.toString());
+        assertThat(result, is("Updated on 26 Aug"));
+
+        dateTime = new DateTime();
+        dateTime = dateTime.minus(new Period(2,0,0,0,0,0,0,0));
+        result = Utils.formatDate(dateTime.toString());
+        assertThat(result, is("Updated on 26 Oct 2015"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_formatDate_invalid_arg(){
+        Utils.formatDate("invalid date");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_formatDate_empty_arg(){
+        Utils.formatDate("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_formatDate_null_arg(){
+        Utils.formatDate(null);
+    }
+
+
+}
